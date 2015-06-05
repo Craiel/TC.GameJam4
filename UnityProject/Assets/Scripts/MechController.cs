bbb9e1d7ce6ec835d69ad8f557eff242f70e609a
Craiel <loft45@gmail.com>
2015-06-05 17:41:43 -0400

    controller sprite

---
+++ UnityProject/Assets/Scripts/MechController.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(CharacterController))]
+    public class MechController : MonoBehaviour
+    {
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        [SerializeField]
+        public float speed;
+
+        // -------------------------------------------------------------------
+        // Private
+        // -------------------------------------------------------------------
+        private CharacterController characterController;
+
+        private void Start()
+        {
+            this.characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
