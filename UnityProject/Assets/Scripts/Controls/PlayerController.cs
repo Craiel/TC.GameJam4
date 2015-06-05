364e232ce06c5732b3ef5a9a831e7be8213ee8fd
Craiel <loft45@gmail.com>
2015-06-05 18:27:45 -0400

    Adding basic movement controller

---
+++ UnityProject/Assets/Scripts/Controls/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿namespace Assets.Scripts.Controls
+{
+    using System;
+
+    using JetBrains.Annotations;
+
+    using UnityEngine;
+
+    public class PlayerController : MonoBehaviour
+    {
+        private const float DefaultSpeedMultiplier = 0.02f;
+
+        private const float DefaultRotationMultiplier = 2f;
+
+        private Vector3 moveDirection;
+
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        [SerializeField]
+        public float velocity = 1.0f;
+
+        [SerializeField]
+        public float rotationSpeed = 1.0f;
+
+        [SerializeField]
+        public bool invertRotationAxis = true;
+        
+        // -------------------------------------------------------------------
+        // Private
+        // -------------------------------------------------------------------
+        private CharacterController characterController;
+
+        [UsedImplicitly]
+        private void Start()
+        {
+            this.characterController = this.GetComponent<CharacterController>();
+
+            System.Diagnostics.Trace.Assert(this.characterController != null);
+
+            this.moveDirection = new Vector3(0, 1, 0);
+        }
+
+        [UsedImplicitly]
+        private void Update()
+        {
+            float move = (Input.GetAxis("Move")) * this.velocity * DefaultSpeedMultiplier;
+            float rotate = (Input.GetAxis("Rotate") * DefaultRotationMultiplier) * this.rotationSpeed;
+
+            if (this.invertRotationAxis)
+            {
+                rotate *= -1;
+            }
+
+            if (Math.Abs(rotate) > float.Epsilon)
+            {
+                this.transform.Rotate(Vector3.forward, rotate);
+            }
+
+            if (Math.Abs(move - float.Epsilon) > float.Epsilon) {
+                this.transform.Translate(this.moveDirection * move);
+            }
+        }
+    }
+}
