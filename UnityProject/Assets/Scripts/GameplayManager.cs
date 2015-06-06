3d8cda1b6b1be57a5e4ef492293d08f4c1478003
Adam Blahuta <adam@jellyfishgames.ca>
2015-06-06 02:57:10 -0400

    + Added GameplayManager + Added spawnpoint parsing to text levels

---
+++ UnityProject/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameplayManager : MonoBehaviour 
+{
+    [SerializeField]
+    private List<Object> textArenas;
+
+    [SerializeField]
+    private List<Texture2D> imageArenas;
+
+    [SerializeField]
+    private Arena arena;
+
+    private List<Vector3> currentSpawnPoints;
+
+    private void Start()
+    {
+        ChooseArena();
+    }
+
+    public void ChooseArena()
+    {
+        int chosenIndex = Random.Range(0, textArenas.Count + imageArenas.Count);
+
+        if (chosenIndex < textArenas.Count)
+        {
+            arena.InitFromText(textArenas[chosenIndex].name, out currentSpawnPoints);
+        }
+        else
+        {
+            arena.InitFromTexture(imageArenas[chosenIndex - textArenas.Count], out currentSpawnPoints);
+        }
+
+        Debug.Log("Spawn Points Count: " + currentSpawnPoints.Count);
+    }
+}
