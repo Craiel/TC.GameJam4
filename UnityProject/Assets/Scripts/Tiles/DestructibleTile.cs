4bec1708d8b82a0632a50f49c4bf7cb428b174f7
Adam Blahuta <adam@jellyfishgames.ca>
2015-06-05 20:13:31 -0400

    + Added destructible tiles + Added half-walls

---
+++ UnityProject/Assets/Scripts/Tiles/DestructibleTile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DestructibleTile : MonoBehaviour 
+{
+    [SerializeField]
+    private float hitPoints;
+
+    private float currentHitPoints;
+
+    private void Awake()
+    {
+        currentHitPoints = hitPoints;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: Implement Projectile collision detection
+    }
+
+    private void TakeDamage(float damage)
+    {
+        currentHitPoints -= damage;
+        if(currentHitPoints <= 0f)
+        {
+            Destroy();
+        }
+    }
+
+    private void Destroy()
+    {
+        //TODO: Implement destroyed vs. undestroyed 
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
