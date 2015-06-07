a4d317c9ab8b8fa6e24f891c78a7608815d89038
Andy <slyefearr@gmail.com>
2015-06-06 20:31:58 -0400

    beam weapon ( not dmg calc yet )

--- UnityProject/Assets/Scripts/Weapons/WeaponColumn.cs
+++ UnityProject/Assets/Scripts/Weapons/WeaponColumn.cs
@@ -23,7 +23,7 @@
             var stats = new StatDictionary
                 {
                     { StatType.Velocity, 0.1f },
-                    { StatType.ProjectileLifeSpan, 1f },
+                    { StatType.ProjectileLifeSpan, .001f },
                     { StatType.Interval, 0.1f }
                 };
 
@@ -47,36 +47,44 @@
             RaycastHit2D ray = Physics2D.Raycast(origin.transform.position, forward);
             Debug.DrawRay(origin.transform.position, forward, Color.red);
             origin.layer = 0;
-            
             if (ray.collider != null)
             {
-                Vector3 depthOffset = new Vector3(0, 0, -1f);
-                LineRenderer line = origin.GetComponent<LineRenderer>();
-                line.SetPosition(0, origin.transform.position + depthOffset);
-                line.SetPosition(1, ray.transform.position + depthOffset);
-                GameObject instance = (GameObject)Object.Instantiate(Resources.Load("Projectiles/EnergyBullet"), origin.transform.position, origin.transform.rotation);
-                instance.GetComponent<Transform>().localScale = new Vector3(1,10,0);
-                //line.SetPosition(1, new Vector3(0f,ray.transform.position.y,0f));
+                //Vector3 depthOffset = new Vector3(0, 0, -1f);
+                //LineRenderer line = origin.GetComponent<LineRenderer>();
+                //line.SetPosition(0, origin.transform.position + depthOffset);
+                //line.SetPosition(1, ray.transform.position + depthOffset);
+                //Debug.Log(origin.name);
+                GameObject instance = (GameObject)Object.Instantiate(Resources.Load("Projectiles/EnergyBullet"), Vector3.zero, origin.transform.rotation);
+                Vector3 scale = new Vector3(1, ray.distance, 0);
+                Vector3 directionOffset = new Vector3(0, ray.distance / 2, 0);
+                instance.transform.position = origin.transform.position;
+                instance.transform.Translate(directionOffset);
+                instance.transform.localScale = scale;
+                instance.AddComponent<BoxCollider2D>();
+                instance.GetComponent<BoxCollider2D>().isTrigger = true;
+                instance.GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
+                StaticProjectileBehavior behavior = instance.AddComponent<StaticProjectileBehavior>();
+                behavior.DamageInfo = new CombatInfo
+                {
+                    Damage = this.GetInternalStat(StatType.Damage),
+                    Type = this.DamageType
+                };
+                behavior.LifeSpan = Time.time + .5f; //Time.time + this.GetInternalStat(StatType.ProjectileLifeSpan);
+                behavior.Origin = origin;
+
                 timeChanged = Time.time;
+                return new List<ProjectileBehavior> { behavior };
             }
-
-            //GameObject instance = (GameObject)Object.Instantiate(this.projectilePrefab, origin.transform.position, origin.transform.rotation);
-
-            //ProjectileBehavior behavior = instance.AddComponent<ProjectileBehavior>();
-            //behavior.Damage = this.GetStat(StatType.Damage);
-            //behavior.Velocity = this.GetStat(StatType.Velocity);
-            //behavior.LifeSpan = Time.time + this.GetStat(StatType.ProjectileLifeSpan);
-
             return null;
         }
 
         public override void Update(GameObject origin)
         {
-            if (timeChanged + .1 <= Time.time)
-            {
-                origin.GetComponent<LineRenderer>().SetPosition(0, new Vector3(0f, 0f, 0f));
-                origin.GetComponent<LineRenderer>().SetPosition(1, new Vector3(0f, 0f, 0f));
-            }
+            //if (timeChanged + .1 <= Time.time)
+            //{
+            //    origin.GetComponent<LineRenderer>().SetPosition(0, new Vector3(0f, 0f, 0f));
+            //    origin.GetComponent<LineRenderer>().SetPosition(1, new Vector3(0f, 0f, 0f));
+            //}
                 
         }
     }
